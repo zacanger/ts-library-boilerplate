c1bae09783972e87c889346adfc095def0fcebd1
zacanger <zac@zacanger.com>
2019-05-03 09:54:46 -0600

    Cleanup

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,17 @@
 /* eslint-env jest */
 
-import { id } from './'
+import { id, keep } from './'
 
-describe('id', (): void => {
-  it('is the identity function', (): void => {
-    expect(id(1)).toBe(1)
+describe('index', (): void => {
+  describe('id', (): void => {
+    it('is the identity function', (): void => {
+      expect(id(1)).toBe(1)
+    })
+  })
+
+  describe('keep', (): void => {
+    it('keeps truthy items', (): void => {
+      expect(keep([false, true, 0, 1, '', 'asdf'])).toEqual([true, 1, 'asdf'])
+    })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export const id = <T>(a: T): T => a
+
+export const keep = <T>(xs: Array<T>): Array<T> => xs.filter(id)
